c4358467b65289445bf94dc0ee735b58805e729c
Maheresio <zamalek.545r5@gmail.com>
2026-07-23 09:45:00 +0200

    Add app constants, colors, strings, and theme styles

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const primaryColor = Color(0xFF4e9bde);
+  static const secondaryColor = Color.fromARGB(255, 105, 175, 233);
+  static const lightWhiteColor = Color.fromARGB(255, 243, 235, 235);
+  static const crystalColor = Color(0xff3a88da);
+}
