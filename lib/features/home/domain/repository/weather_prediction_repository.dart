4d472862fa85e261eed3246ba2bd63aaae82effd
Maheresio <zamalek.545r5@gmail.com>
2026-07-24 18:37:26 +0300

    refactor: remove deprecated weather prediction feature
    - Delete prediction data source, repository, usecase, cubit, and state
    - Remove old weather_provider

--- lib/features/home/domain/repository/weather_prediction_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-import '../../../../core/error/failure.dart';
-
-abstract class WeatherPredictionRepository {
-  Future<Either<Failure, int>> getWeatherPrediction(List<int> features);
-}
